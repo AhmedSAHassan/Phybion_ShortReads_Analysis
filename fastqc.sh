84d266568b6e0ba8a208117db46f362c4731eb44
Ahmed Hassan <ahmedsalahtvi@gmail.com>
2025-01-10 15:51:41 +0100

    updating

--- fastqc.sh
+++ fastqc.sh
@@ -8,21 +8,3 @@ for sampleID in Zahnreich_FASTQ_20240701/*.fastq.gz; do
 done
 echo "fastqc finished running!"
 
-
-for sampleID in trimmed/t_*/*paired.fq.gz; do
-    sample_name=$(basename "$sampleID" "paired.fq.gz")
-    echo "working on $sample_name"
-    mkdir -p ./fastqc_trimmed/"$sample_name"
-    fastqc "$sampleID" -o ./fastqc_trimmed/"$sample_name"/
-done
-echo "fastqc finished running!"
-
-
-
-for sampleID in fastp/t_*/*.fq.gz; do
-    sample_name=$(basename "$sampleID" ".fq.gz")
-    echo "working on $sample_name"
-    mkdir -p ./fastqc_fastp/"$sample_name"
-    fastqc "$sampleID" -o ./fastqc_fastp/"$sample_name"/
-done
-echo "fastqc finished running!"
